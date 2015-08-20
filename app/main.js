15457ad2273cb548592601225ccc1381bbd5e8f1
masuda_daiju <masuda_daiju@aratana.jp>
2015-08-20 22:40:26 +0900

    Adjust indent of main.js.

--- app/main.js
+++ app/main.js
@@ -2,30 +2,22 @@ var app = require('app');
 var BrowserWindow = require('browser-window'); 
 var mainWindow = null;
 
-/**
- * quit when all window are closed
- **/
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
     app.quit();
-  }
 });
 
-/**
- * when electron finished initialization
- **/
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({
-    'width': 800,
-    'height': 600,
-    'min-width': 400,
-    'min-height': 400,
-    'auto-hide-menu-bar': true
-  });
+    mainWindow = new BrowserWindow({
+        'width': 800,
+        'height': 600,
+        'min-width': 400,
+        'min-height': 400,
+        'auto-hide-menu-bar': true
+    });
 
-  mainWindow.loadUrl('file://' + __dirname + '/imageviewer.html');
+    mainWindow.loadUrl('file://' + __dirname + '/imageviewer.html');
   
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
 });
